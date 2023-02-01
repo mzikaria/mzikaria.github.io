5cfde6e856410af06b0f132889904cfa0cd8d99a
Mariam Zikaria <mzikaria@gmail.com>
2023-01-31 23:13:18 -0600

    Add placeholder for experience timeline

--- src/components/pages/home/index.js
+++ src/components/pages/home/index.js
@@ -3,12 +3,16 @@ import './index.css';
 
 import AboutSection from '../../sections/about';
 import ProjectsSection from '../../sections/projects';
+import WorkSection from '../../sections/work';
 
 const Home = () => {
   return (
     <div className="w-full flex justify-center flex-col items-center">
       <AboutSection />
+      <hr className="h-px my-8 bg-gray-200 w-full border-0 dark:br-gray-700"/>
       <ProjectsSection />
+      <hr className="h-px my-8 bg-gray-200 w-full border-0 dark:br-gray-700"/>
+      <WorkSection />
     </div>
   );
 }
--- src/components/sections/work/index.js
+++ src/components/sections/work/index.js
@@ -1,10 +1,64 @@
 import React from 'react';
 import './index.css';
 
+const Experience = ({ title, description, isOdd }) => {
+  if (isOdd) {
+    return (
+      <div class="container left">
+        <div class="content">
+          <h2>{title}</h2>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div class="container right">
+      <div class="content">
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+
 const WorkSection = () => {
+  const experiences = [
+    {
+      title: 'This is the title',
+      description: 'This is the description',
+    },
+    {
+      title: 'This is the title',
+      description: 'This is the description',
+    },
+    {
+      title: 'This is the title',
+      description: 'This is the description',
+    },
+    {
+      title: 'This is the title',
+      description: 'This is the description',
+    }
+  ]
+
   return (
     <div id="work-section" className="w-2/3">
-
+      <div class="timeline">
+        {
+          experiences.map((experience, index) => {
+            return (
+              <Experience
+                isOdd={index % 2 !== 0}
+                title={experience.title}
+                description={experience.description}
+              />
+            )
+          })
+        }
+      </div>
     </div>
   );
 }
